dd693e27263d9d2863dd96f901224e5b2885158a
Emilly Cristina <emilly.c.f@hotmail.com>
2023-11-27 00:31:35 -0300

    update

--- src/components/FormUser/FormUser.jsx
+++ src/components/FormUser/FormUser.jsx
@@ -13,37 +13,47 @@ import {
 import React, { useState } from "react";
 import { MdAddAPhoto } from "react-icons/md";
 
+
+
 export const FormUser = () => {
+  const [cep, setCep] = useState("");
+  const [endereco, setEndereco] = useState({
+    logradouro: "",
+    bairro: "",
+    cidade: "",
+    uf: "",
+  });
+
   const [imagemPerfil, setImagemPerfil] = useState(null);
   const [estadoSelecionado, setEstadoSelecionado] = useState("");
   const estadosBrasileiros = [
-    "Acre",
-    "Alagoas",
-    "Amapá",
-    "Amazonas",
-    "Bahia",
-    "Ceará",
-    "Distrito Federal",
-    "Espírito Santo",
-    "Goiás",
-    "Maranhão",
-    "Mato Grosso",
-    "Mato Grosso do Sul",
-    "Minas Gerais",
-    "Pará",
-    "Paraíba",
-    "Paraná",
-    "Pernambuco",
-    "Piauí",
-    "Rio de Janeiro",
-    "Rio Grande do Norte",
-    "Rio Grande do Sul",
-    "Rondônia",
-    "Roraima",
-    "Santa Catarina",
-    "São Paulo",
-    "Sergipe",
-    "Tocantins",
+    "AC",
+    "AL",
+    "AP",
+    "AM",
+    "BA",
+    "CE",
+    "DF",
+    "ES",
+    "GO",
+    "MA",
+    "MT",
+    "MS",
+    "MG",
+    "PA",
+    "PB",
+    "PR",
+    "PE",
+    "PI",
+    "RJ",
+    "RN",
+    "RS",
+    "RO",
+    "RR",
+    "SC",
+    "SP",
+    "SE",
+    "TO",
   ];
 
   const handleImagemPerfilChange = (e) => {
@@ -56,6 +66,29 @@ export const FormUser = () => {
       };
       reader.readAsDataURL(arquivo);
     }
+  };
+
+  const handleCepChange = (e) => {
+    const cepNovo = e.target.value;
+
+    if (cepNovo.length === 8) {
+      fetch(`https://viacep.com.br/ws/${cepNovo}/json/`)
+        .then((response) => response.json())
+        .then((data) => {
+          setEndereco({
+            logradouro: data.logradouro,
+            bairro: data.bairro,
+            cidade: data.localidade,
+            uf: data.uf,
+          });
+          setEstadoSelecionado(data.uf); 
+        })
+        .catch(error => {
+          console.error('Erro ao buscar CEP', error)
+        })
+        
+      }
+     setCep(cepNovo)
   };
 
   return (
@@ -73,6 +106,7 @@ export const FormUser = () => {
           width: "10rem",
           height: "10rem",
           position: "relative",
+          zIndex: 0,
         }}
       >
         <Input
@@ -87,7 +121,9 @@ export const FormUser = () => {
             sx={{
               position: "absolute",
               bottom: "8px",
-              right: "17px",
+              right: "20px",
+              zIndex: "1",
+              
             }}
             component="span"
           >
@@ -107,34 +143,47 @@ export const FormUser = () => {
 
         <TextField label="Telefone" variant="standard" type="text"></TextField>
 
-        <TextField label="CEP" variant="standard" type="text"></TextField>
+        <TextField label="CEP" 
+        variant="standard" 
+        value={cep}
+        onChange={handleCepChange}
+        type="text">
+        
+        </TextField>
+
       </Box>
       <Box sx={{ marginBottom: "15px" }}>
         <TextField
           label="Logradouro"
+          value={endereco.logradouro}
           variant="standard"
           type="text"
         ></TextField>
 
-        <TextField label="Bairro" variant="standard" type="text"></TextField>
+        <TextField label="Bairro" 
+        variant="standard" 
+        value={endereco.bairro}
+        type="text"></TextField>
 
-        <TextField label="Cidade" variant="standard" type="text"></TextField>
+        <TextField label="Cidade" 
+        variant="standard" 
+        value={endereco.cidade}
+        type="text"></TextField>
 
-
-      <FormControl variant="standard" sx={{ m: 1, minWidth: 200 }}>
-        <InputLabel id="demo-simple-select-standard-label">UF</InputLabel>
-        <Select
-          value={estadoSelecionado}
-          onChange={(e) => setEstadoSelecionado(e.target.value)}
-        >
-          <MenuItem value="">Selecione um estado</MenuItem>
-          {estadosBrasileiros.map((estado) => (
-            <MenuItem key={estado} value={estado}>
-              {estado}
-            </MenuItem>
-          ))}
-        </Select>
-      </FormControl>
+        <FormControl variant="standard" sx={{ m: 1, minWidth: 200 }}>
+          <InputLabel id="demo-simple-select-standard-label">UF</InputLabel>
+          <Select
+            value={estadoSelecionado}
+            onChange={(e) => setEstadoSelecionado(e.target.value)}
+          >
+            <MenuItem value="">Selecione um estado</MenuItem>
+            {estadosBrasileiros.map((estado) => (
+              <MenuItem key={estado} value={estado}>
+                {estado}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
       </Box>
 
       <Box
