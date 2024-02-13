0f0c27c5a4b1c99a9f007026410764b267d3948f
Emilly Cristina <emilly.c.f@hotmail.com>
2024-02-13 17:55:33 -0300

    update

--- next.config.js
+++ next.config.js
@@ -4,7 +4,8 @@ const nextConfig = {
         return [
           {
             source: '/',
-            destination: '/Login', // Substitua 'sua-rota-personalizada' pelo caminho desejado
+            //destination: '/Login',
+            destination: 'https://finances-front-gilt.vercel.app/Login', // Substitua 'sua-rota-personalizada' pelo caminho desejado
             permanent: true, // Define o redirecionamento como permanente (HTTP 301)
           },
         ];
