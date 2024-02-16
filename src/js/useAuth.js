47f2287afe4eeee2890caeef37ef87d492a13760
Emilly Cristina <emilly.c.f@hotmail.com>
2024-02-16 17:28:38 -0300

    update

--- src/js/useAuth.js
+++ src/js/useAuth.js
@@ -1,22 +1,20 @@
-
-import { getSession } from 'next-auth/react';
-import { toast } from 'react-toastify';
+// Importe o useRouter do next/router
+import { useRouter } from 'next/router';
 
 const authMiddleware = async (handler) => {
   return async (req, res) => {
     const session = await getSession({ req });
 
-    
-
     if (!session) {
-        
-        toast.warn('Você precisa fazer login para acessar esta página');
-      return {
-        redirect: {
-          destination: '/Login',
-          permanent: false,
-        },
-      };
+      // Exibe o toast
+      toast.warn('Você precisa fazer login para acessar esta página');
+
+      // Redireciona no cliente
+      const router = useRouter();
+      router.push('/Login');
+
+      // Retorna vazio para evitar a execução adicional do handler
+      return {};
     }
 
     return handler(req, res);
